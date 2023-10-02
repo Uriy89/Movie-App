44880cdc27e5f56b267e01b0ecdd26971062255b
Юрий Аверьянов <averianovj@gmail.com>
2023-10-02 22:52:29 +0300

    view movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,11 @@ import MovieBlock from '../MovieBlock';
 import SearchInput from '../SearchInput';
 import './App.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
   render() {
     return (
-      // eslint-disable-next-line react/jsx-filename-extension
       <div className="wrapper">
         <SearchInput />
-        <MovieBlock />
-        <MovieBlock />
-        <MovieBlock />
-        <MovieBlock />
-        <MovieBlock />
-        <MovieBlock />
-        <MovieBlock />
         <MovieBlock />
       </div>
     );
--- src/components/MovieBlock/MovieBlock.js
+++ src/components/MovieBlock/MovieBlock.js
@@ -7,46 +7,46 @@ class MovieBlock extends Component {
   movieService = new MovieService();
 
   state = {
-    title: null,
-    release: null,
-    description: null
+    movies: []
   };
 
   constructor() {
     super();
-    this.updateMovie();
+    this.allMovie();
   }
 
-  updateMovie() {
-    this.movieService.getMoviePopular().then((movie) => {
-      this.setState({
-        title: movie.title,
-        release: movie.release_date,
-        description: movie.overview
-      });
-    });
+  onMoviesLoaded = (movies) => {
+    this.setState({ movies });
+    console.log(movies);
+  };
+
+  allMovie() {
+    this.movieService.getMoviePopular().then(this.onMoviesLoaded);
   }
 
   render() {
-    const { title, release, description } = this.state;
+    const elements = this.state.movies.map((item) => {
+      const { title, release, description } = item;
 
-    // eslint-disable-next-line react/jsx-filename-extension
-    return (
-      // eslint-disable-next-line react/jsx-no-comment-textnodes
-      <div className="movie-block">
-        <img src={m} alt="movie cover" />
-        <div className="block-info">
-          <div className="block-info__top">
-            <h3 className="block-info__tile">{title}</h3>
-            <div className="block-info__rating">6.6</div>
+      return (
+        // eslint-disable-next-line react/jsx-key
+        <div className="movie-block">
+          <img src={m} alt="movie cover" />
+          <div className="block-info">
+            <div className="block-info__top">
+              <h3 className="block-info__tile">{title}</h3>
+              <div className="block-info__rating">6.6</div>
+            </div>
+            <p className="block-info__release">{release}</p>
+            <span className="block-info__genre">Action</span>
+            <span className="block-info__genre">Drama</span>
+            <p className="block-info__description">{description}</p>
           </div>
-          <p className="block-info__release">{release}</p>
-          <span className="block-info__genre">Action</span>
-          <span className="block-info__genre">Drama</span>
-          <p className="block-info__description">{description}</p>
         </div>
-      </div>
-    );
+      );
+    });
+
+    return <>{elements}</>;
   }
 }
 
